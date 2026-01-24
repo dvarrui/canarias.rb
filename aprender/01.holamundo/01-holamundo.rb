67503bf83c8c7f68199a9fdfbcfdcf84c06a4143
David Vargas <dvarrui@protonmail.com>
2026-01-24 09:06:40 +0000

    move files to other repo

--- aprender/01.holamundo/01-holamundo.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-name = "Obiwan"
-age = 57
-height = 180
-
-puts "El personaje #{name}, tiene #{age} años de edad y mide #{180} metros."
-
--- aprender/01.holamundo/02-holamundo.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-print("Name: "); name = gets.chomp
-print("Age: "); age = gets.to_i
-print("Height: "); height = gets.to_i
-
-puts "El personaje #{name}, tiene #{age} años de edad y mide #{180} metros."
